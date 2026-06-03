a1bfc4b46a4f2a85a432688e4afb992ef756d9c8
Renee <rjtung15@gmail.com>
2026-06-02 22:09:20 -0400

    model training on multiple loads

---
+++ rate/train_multiload_models.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# train models
+for i in {1..3}
+do
+    echo "Running iteration $i..."
+    python main_multiload.py --gpu 0 --gpu_frac 0.20 --n_trials 40000 --mode train \
+    --N 1000 --P_inh 0.20 --som_N 0 --apply_dale True --gain 1.5 --task sternberg \
+    --task_load 1 2 3 --eval_thresholds 0.95 0.9 0.8 --delay_dur 50 \
+    --act sigmoid --loss_fn l2 --decay_taus 4 150 --jitter_onset 5 --jitter_delay 3 \
+    --output_dir ~/Documents/renee/
+    echo "Iteration $i complete."
+done
+
+
+# python main_interleaved.py --gpu 0 --gpu_frac 0.20 --n_trials 40000 --mode train \
+# --N 1000 --P_inh 0.20 --som_N 0 --apply_dale True --gain 1.5 --task sternberg \
+# --load_proportion 0.5 --delay_dur 50 \
+# --act sigmoid --loss_fn l2 --decay_taus 4 150 --jitter_onset 5 --jitter_delay 3 \
+# --output_dir ~/Documents/renee/
